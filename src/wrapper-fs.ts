460aa9fe758ad73de8309b201bb33ee58c44c7ca
ottyajp <ottyajp@gmail.com>
2024-02-21 00:47:01 +0900

    インデント幅を2から4に変更
    せまくて見づらかった

--- src/wrapper-fs.ts
+++ src/wrapper-fs.ts
@@ -2,29 +2,29 @@ import fs, { Dirent } from 'fs';
 import { Dictionary } from './types/Dictionary';
 
 export async function readdir(path: string): Promise<Dirent[]> {
-  return new Promise((resolve, reject) => {
-    fs.readdir(
-      path,
-      { withFileTypes: true, recursive: false },
-      (err, files) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(files);
-        }
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        fs.readdir(
+            path,
+            { withFileTypes: true, recursive: false },
+            (err, files) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(files);
+                }
+            }
+        );
+    });
 }
 
 export async function readDictionary(path: string): Promise<Dictionary> {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, (err: NodeJS.ErrnoException | null, data: Buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(JSON.parse(data.toString()) as Dictionary);
-      }
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err: NodeJS.ErrnoException | null, data: Buffer) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(data.toString()) as Dictionary);
+            }
+        });
     });
-  });
 }
